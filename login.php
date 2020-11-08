10282d12a73729fde0c7945f195eacf4e10d9711
CarlosAlb7 <71297869+CarlosAlb7@users.noreply.github.com>
2020-11-08 15:58:21 -0800

    Loginphp

--- login.php
+++ login.php
@@ -1,13 +1,52 @@
 <?php
 
+  session_start();
 
-$conexion=mysqli_connect('localhost','root','','basewebsite') or die ('Error en la base de datos');
+  if (isset($_SESSION['user_id'])) {
+    header('Location: /php-login');
+  }
+  require 'database.php';
 
-$sql="INSERT INTO tablaslogin VALUES(null,'".$_POST["user"]."','".$_POST["password"]."')";
-$resultado=mysqli_query($conexion,$sql) or die ('Error en el query');
-mysqli_close($conexion);
-echo "¡El cuestionario ha sido contestado correctamente!"
-	
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    $records->bindParam(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
 
+    $message = '';
+
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+      $_SESSION['user_id'] = $results['id'];
+      header("Location: /php-login");
+    } else {
+      $message = 'Sorry, those credentials do not match';
+    }
+  }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aerobionics</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Inicia</h1>
+    <span>or <a href="signup.php">Registrate</a></span>
+
+    <form action="login.php" method="POST">
+      <input name="email" type="text" placeholder="Enter your email">
+      <input name="password" type="password" placeholder="Enter your Password">
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
